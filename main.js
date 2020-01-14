8a387a47184c6d10d3272beeba2a9f28247dc0d7
Anthony Kuske <anthonykuske@gmail.com>
2020-01-14 14:30:14 +0000

    Add main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+module.exports = {
+    StarBarcodeEncode: require('./Sample/js/StarBarcodeEncoder'),
+    StarWebPrintBuilder: require('./Sample/js/StarWebPrintBuilder'),
+    StarWebPrintDisplayBuilder: require('./Sample/js/StarWebPrintDisplayBuilder'),
+    StarWebPrintExtManager: require('./Sample/js/StarWebPrintExtManager'),
+    StarWebPrintTrader: require('./Sample/js/StarWebPrintTrader'),
+};
